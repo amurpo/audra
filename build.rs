d4fea1a589099b6e1da7d61965860aa43511d986
Daniel Avila <54181842+amurpo@users.noreply.github.com>
2026-05-16 22:48:56 -0400

    Update build.rs add for windows build

--- build.rs
+++ build.rs
@@ -7,8 +7,11 @@ fn main() {
 
     let out_dir = env::var("OUT_DIR").unwrap();
     let proxy_url = env::var("LASTFM_PROXY_URL").unwrap_or_default();
-
     let content = format!("pub const PROXY_URL: &str = \"{}\";\n", proxy_url);
-
     fs::write(Path::new(&out_dir).join("credentials_gen.rs"), content).unwrap();
+
+    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
+        println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
+    }
 }
